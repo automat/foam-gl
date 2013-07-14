e2cf44090d81fb448c5d5ca1c99ac9ab659b9944
Henryk Wollik <olafyoorsen@hotmail.com>
2013-07-14 17:17:38 +0200

    + emission

--- src/glKit/graphics/gl/glkMaterial.js
+++ src/glKit/graphics/gl/glkMaterial.js
@@ -1,15 +1,21 @@
-GLKit.Material = function(ambient,diffuse,specular,shininess)
+GLKit.Material = function(ambient,diffuse,specular,shininess,emission)
 {
-    ambient   = ambient || GLKit.Color.make(1.0,0.5,0.5,1.0);
-    diffuse   = diffuse || GLKit.Color.BLACK();
-    specular  = specular || GLKit.Color.BLACK();
+    ambient   = ambient   || GLKit.Color.make(1.0,0.5,0.5,1.0);
+    diffuse   = diffuse   || GLKit.Color.BLACK();
+    specular  = specular  || GLKit.Color.BLACK();
     shininess = shininess || 1.0;
+    emission  = emission  || GLKit.Color.BLACK;
 
+    this.emission  = emission;
     this.ambient   = ambient;
     this.diffuse   = diffuse;
     this.specular  = specular;
-    this._shininess = shininess;
+    this.shininess = shininess;
 };
+
+GLKit.Material.prototype.setEmission   = function(color)  {this.emission = color;};
+GLKit.Material.prototype.setEmission3f = function(r,g,b)  {this.emission[0] = r;this.emission[1] = g;this.emission[2] = b;};
+GLKit.Material.prototype.setEmission4f = function(r,g,b,a){this.emission[0] = r;this.emission[1] = g;this.emission[2] = b;this.emission[3] = a;};
 
 GLKit.Material.prototype.setAmbient   = function(color)  {this.ambient = color;};
 GLKit.Material.prototype.setAmbient3f = function(r,g,b)  {this.ambient[0] = r;this.ambient[1] = g;this.ambient[2] = b;};
@@ -23,9 +29,9 @@ GLKit.Material.prototype.setSpecular   = function(color)  {this.specular = color
 GLKit.Material.prototype.setSpecular3f = function(r,g,b)  {this.specular[0] = r;this.specular[1] = g;this.specular[2] = b;};
 GLKit.Material.prototype.setSpecular4f = function(r,g,b,a){this.specular[0] = r;this.specular[1] = g;this.specular[2] = b;this.specular[3] = a;};
 
-GLKit.Material.prototype.setShininess  = function(shininess){this._shininess = shininess};
 
+GLKit.Material.prototype.getEmission  = function(){return GLKit.Color.copy(this.emission);};
 GLKit.Material.prototype.getAmbient   = function(){return GLKit.Color.copy(this.ambient);};
 GLKit.Material.prototype.getDiffuse   = function(){return GLKit.Color.copy(this.diffuse);};
 GLKit.Material.prototype.getSpecular  = function(){return GLKit.Color.copy(this.specular);};
-GLKit.Material.prototype.getShininess = function(){return this._shininess;};
+GLKit.Material.prototype.getShininess = function(){return this.shininess;};
