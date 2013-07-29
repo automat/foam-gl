1ce0749c935121e4fe458e5a4218eb3fe5c6d224
Henryk Wollik <olafyoorsen@hotmail.com>
2013-07-29 20:40:58 +0200

    ...

--- src/glKit/graphics/gl/shader/glkProgVertexShader.js
+++ src/glKit/graphics/gl/shader/glkProgVertexShader.js
@@ -16,12 +16,13 @@ GLKit.ProgVertexShader=
 
 "uniform float uPointSize;" + 
 
+
 "void main()" + 
 "{" + 
-"    vVertexPosition = uModelViewMatrix * vec4(aVertexPosition,1.0);" + 
-"    vVertexNormal   = aVertexNormal;" + 
-"    vVertexColor    = aVertexColor;" + 
-"    vVertexUV       = aVertexUV;" + 
+"    vVertexPosition  = uModelViewMatrix * vec4(aVertexPosition,1.0);" + 
+"    vVertexNormal    = aVertexNormal;" + 
+"    vVertexColor     = aVertexColor;" + 
+"    vVertexUV        = aVertexUV;" + 
 
 "    gl_Position  = uProjectionMatrix * vVertexPosition;" + 
 "    gl_PointSize = uPointSize;" + 
