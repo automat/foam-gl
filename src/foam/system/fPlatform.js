c4a208a19a701a3ea3f499a38fbf5996cd83890e
Henryk Wollik <olafyoorsen@hotmail.com>
2013-10-01 09:18:47 +0200

    added NodeWebkit target

--- src/foam/system/fPlatform.js
+++ src/foam/system/fPlatform.js
@@ -1,11 +1,25 @@
-var Platform = {WEB:0,PLASK:1};
+var Platform = {WEB:0,PLASK:1,NODE_WEBKIT:2};
     Platform._target = null;
 
 Platform.getTarget  = function()
 {
-    if(!this._target)this._target = (typeof window !== 'undefined' && typeof document !== 'undefined') ? this.WEB :
-                                    (typeof require == "function" && require) ? this.PLASK :
-                                     null;
+    //TODO fix&finish
+    if(!this._target)
+    {
+        var bWindow   = typeof window !== 'undefined',
+            bDocument = typeof document !== 'undefined',
+            bRequireF = typeof require == 'function',
+            bRequire  = !!require,
+            bProcess  = typeof process !== 'undefined';
+
+        console.log(bWindow,bDocument,bRequireF,bRequire,bProcess);
+
+        this._target = (bWindow && bDocument) ? this.WEB :
+                       (bWindow && bDocument && bRequireF && bRequire) ? this.NODE_WEBKIT :
+                       (bRequireF && bRequire) ? this.PLASK :
+                       null;
+    }
+
     return this._target;
 };
 
