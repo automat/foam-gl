6f8f63e52e838461f6878ee3fd432d0e2d5cca3c
Henryk Wollik <olafyoorsen@hotmail.com>
2013-10-12 22:29:15 +0200

    added System Impl Plask

--- src/foam/system/fSystemImplPlask.js
+++ src/foam/system/fSystemImplPlask.js
@@ -1,3 +1,57 @@
-var SystemImplPlask = {};
+var fs     = require('fs'),
+    path   = require('path'),
+    fError = require('./common/fError');
+
+var SystemImplPlask =
+{
+    readFile : function(file,callback)
+    {
+        fs.readFile(file,'utf8',function(err,data)
+        {
+            if(err)throw err;
+            callback(data);
+        });
+
+        return true;
+    },
+
+    readFileSync : function(file){return fs.readFileSync(file,'utf8');},
+
+    writeFile : function(filepath,data,callback)
+    {
+        if(!fs.existsSync(this.getDirectory(filepath)))throw Error(fError.DIRECTORY_DOESNT_EXIST);
+        fs.writeFile(filepath,data,'utf8',
+            function(err)
+            {
+                if(err)throw err;
+                callback();
+            });
+
+        return true;
+    },
+
+    writeFileSync : function(filepath,data)
+    {
+        if(!fs.existsSync(this.getDirectory(filepath)))throw Error(fError.DIRECTORY_DOESNT_EXIST);
+        fs.writeFileSync(filepath,data,'utf8');
+        return true;
+    },
+
+    getDirectory : function(file)
+    {
+        file = file || module.parent.parent.parent.parent.filename;
+        return path.dirname(file);
+    },
+
+    getFilePath  : function(file)
+    {
+        var p = path.join(this.getDirectory(),file);
+        return fs.existsSync(p) ? p : null;
+    },
+
+    makeFilePath : function(filepath){return path.join(this.getDirectory(),filepath)}
+
+
+};
 
 module.exports = SystemImplPlask;
