746deebd0405df0a2a8ad6fcafced89705283c55
Henryk Wollik <olafyoorsen@hotmail.com>
2013-10-14 14:02:38 +0200

    cleanup

--- src/foam/system/fSystemImplWeb.js
+++ src/foam/system/fSystemImplWeb.js
@@ -1,26 +1,3 @@
-/*
-var Application = require('../app/fApplication');
-
-var SystemImplWeb = {};
-
-SystemImplWeb.loadImage = function(imageSrc,callback)
-{
-    var image = new Image();
-        image.addEventListener('load',callback);
-        image.src = imageSrc;
-};
-
-SystemImplWeb.getContext3dData = function()
-{
-   return Application.getInstance().getWindow().toDataURL();
-};
-
-SystemImplWeb.loadFile = function(file,callback)
-{
-
-};
-
-*/
 
 var SystemImplWeb =
 {
