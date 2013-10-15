b5b6d088fceed3b8bb6a79d908fc4b9d51bece1a
Henryk Wollik <olafyoorsen@hotmail.com>
2013-10-15 14:24:32 +0200

    cleanup

--- scripts/buildShader.js
+++ scripts/buildShader.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var inPath  = '../includes/';
     outPath = '../src/foam/graphics/gl/shader/';
 
-
+/*
 function shader2String(shaderSource,dist)
 {
     var string = fs.readFileSync(shaderSource,'utf-8');
@@ -13,13 +13,38 @@ function shader2String(shaderSource,dist)
 
     fs.writeFileSync(dist,'module.exports ="' + string + '";','utf-8');
 }
+*/
 
-shader2String(inPath + '/temp/fProgFragShader.glsl',outPath + 'fProgFragShader.js');
-shader2String(inPath + '/temp/fProgVertShader.glsl',outPath + 'fProgVertShader.js');
-shader2String(inPath + '/temp/fRenderVertShader.glsl',outPath + 'fRenderVertShader.js');
-shader2String(inPath + '/temp/fRenderFragShader.glsl',outPath + 'fRenderFragShader.js');
+function shader2String(files)
+{
+    var i = -1, l = files.length, obj,str;
+    while(++i < l)
+    {
+        obj = files[i];
+        if(!obj.src)throw  'file src missing.';
+        if(!obj.dest)throw 'file dest missing.';
 
-shader2String(inPath + 'fColorFragShader.glsl', outPath + 'fColorFragShader.js');
-shader2String(inPath + 'fColorVertShader.glsl', outPath + 'fColorVertShader.js');
-shader2String(inPath + 'fNormalFragShader.glsl',outPath + 'fNormalFragShader.js');
-shader2String(inPath + 'fNormalVertShader.glsl',outPath + 'fNormalVertShader.js');
+        str = fs.readFileSync(obj.src,'utf-8');
+        str = str.replace(/(\r\n|\n|\r)/gm,"");
+        str = str.replace(/ +(?= )/g,'');
+
+        fs.writeFile(obj.dest,'module.exports ="'+str+'";','utf-8');
+    }
+
+}
+
+shader2String([
+
+    {src: inPath + '/temp/fProgFragShader.glsl',   dest: outPath + 'fProgFragShader.js'},
+    {src: inPath + '/temp/fProgVertShader.glsl',   dest: outPath + 'fProgVertShader.js'},
+    {src: inPath + '/temp/fRenderVertShader.glsl', dest: outPath + 'fRenderVertShader.js'},
+    {src: inPath + '/temp/fRenderFragShader.glsl', dest: outPath + 'fRenderFragShader.js'},
+
+    {src: inPath + '/material/fColorFragShader.glsl',        dest: outPath + 'fColorFragShader.js'},
+    {src: inPath + '/material/fColorVertShader.glsl',        dest: outPath + 'fColorVertShader.js'},
+    {src: inPath + '/material/fColorSolidFragShader.glsl',   dest: outPath + 'fColorSolidFragShader.js'},
+    {src: inPath + '/material/fColorSolidVertShader.glsl',   dest: outPath + 'fColorSolidVertShader.js'},
+    {src: inPath + '/material/fNormalFragShader.glsl',       dest: outPath + 'fNormalFragShader.js'},
+    {src: inPath + '/material/fNormalVertShader.glsl',       dest: outPath + 'fNormalVertShader.js'},
+
+]);
