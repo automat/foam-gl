77065a4168a4eec9cdfb04e104fd80f755186ee8
Henryk Wollik <olafyoorsen@hotmail.com>
2013-10-18 22:47:40 +0200

    added non power of 2 err

--- src/foam/system/common/fError.js
+++ src/foam/system/common/fError.js
@@ -9,5 +9,9 @@ module.exports =
     MATRIX_STACK_POP_ERROR: 'Matrix stack invalid pop.',
     VERTICES_IN_WRONG_SIZE: 'Vertices array has wrong length. Should be ',
     COLORS_IN_WRONG_SIZE:   'Color array length not equal to number of vertices.',
-    DIRECTORY_DOESNT_EXIST: 'File target directory does not exist.'
+    DIRECTORY_DOESNT_EXIST: 'File target directory does not exist.',
+    FILE_DOESNT_EXIST:      'File does not exist.',
+    TEXTURE_WIDTH_NOT_P2:   'Texture imageData is not power of 2.',
+    TEXTURE_HEIGHT_NOT_P2:  'Texture imageData is not power of 2.',
+    TEXTURE_IMAGE_DATA_NULL:'Texture imageData is null.'
 };
