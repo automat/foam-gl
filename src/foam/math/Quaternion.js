a2398bee29d37b8064c56f36534bb789ad368085
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-20 17:52:08 +0200

    rewrite Quaternion initial

--- src/foam/math/Quaternion.js
+++ src/foam/math/Quaternion.js
@@ -1,3 +1,244 @@
+var Vec3 = require('./Vec3'),
+    Matrix44 = require('./Matrix44');
+
+var Quaternion = {
+
+    _EPSIOLON : 0.00001,
+
+    _AXIS_X : Vec3.AXIS_X(),
+    _AXIS_Y : Vec3.AXIS_Y(),
+    _AXIS_Z : Vec3.AXIS_Z(),
+
+    // create
+
+    create : function(){
+       return this.identity(new Float32Array(4));
+    },
+
+    create4f : function(x,y,z,w){
+        return this.set4f(new Float32Array(4),x,y,z,w);
+    },
+
+    createv : function(v,w){
+        return this.setv(new Float32Array(4),v,w);
+    },
+
+    createFromAxis : function(v,angle){
+        return this.setFromAxis(new Float32Array(4),v,angle);
+    },
+
+    createFromTo : function(v0,v1){
+        return this.setFromTo(new Float32Array(4),v0,v1);
+    },
+
+    createFromAngles : function(x,y,z){
+        return this.setFromAngles(new Float32Array(4),x,y,z);
+    },
+
+    createFromMatrix44 : function(m){
+
+    },
+
+    //  set
+
+    identity : function(q){
+        q[0] = q[1] = q[2] = 0;
+        q[3] = 1;
+        return q;
+    },
+
+    set4f : function(q,x,y,z,w){
+        q[0] = x;
+        q[1] = y;
+        q[2] = z;
+        q[3] = w;
+        return q;
+    },
+
+    setv : function(q,v,w){
+        return this.set4f(q,v[0],v[1],v[2],w);
+    },
+
+    setFromTo : function(q,v0,v1){
+        var v0x = v0[0],
+            v0y = v0[1],
+            v0z = v0[2];
+        var v0length = Math.sqrt(v0x * v0x + v0y * v0y + v0z * v0z);
+        var v1x = v1[0],
+            v1y = v1[1],
+            v1z = v1[2];
+        var v1length = Math.sqrt(v1x * v1x + v1y * v1y + v1z * v1z);
+        var ax,ay,az,angle;
+
+        if(v0length){
+            v0length = 1.0 / v0length;
+            v0x *= v0length;
+            v0y *= v0length;
+            v0z *= v0length;
+        }
+
+        if(v1length){
+            v1length = 1.0/ v1length;
+            v1x *= v1length;
+            v1y *= v1length;
+            v1z *= v1length;
+        }
+
+        if(v0x == -v1x &&
+           v0y == -v1y &&
+           v0z == -v1z){
+            angle = Math.abs(Vec3.dot(v0,this._AXIS_X));
+
+            var x, y, z;
+            if(angle < 1.0){
+                x = 1;
+                y = 0;
+                z = 0;
+            } else {
+                x = 0;
+                y = 1;
+                z = 0;
+            }
+                                      /*
+            vo[0] = y0 * z1 - y1 * z0;
+            vo[1] = z0 * x1 - z1 * x0;
+            vo[2] = x0 * y1 - x1 * y0;  */
+
+            ax = v0y * z - y * v0z;
+            ay = v0z * x - z * v0x;
+            az = v0x * y - x * v0y;
+
+            var olength = Math.sqrt(ax * ax + ay * ay + az * az);
+            if(olength){
+                olength = 1.0 / olength;
+                ax *= olength;
+                ay *= olength;
+                az *= olength;
+            }
+
+            return this.setFromAxis3f(q,ax,ay,az,Math.PI)
+        }
+
+        var v01x = v0x + v1x,
+            v01y = v0y + v1y,
+            v01z = v0z + v1z;
+
+        var v01length = Math.sqrt(v01x * v01x + v01y * v01y + v01z * v01z);
+        if(v01length){
+            v01length = 1.0 / v01length;
+            v01x *= v01length;
+            v01y *= v01length;
+            v01z *= v01length;
+        }
+
+        angle = v0x * v01x + v0y * v01y + v0z * v01z;
+        ax = v0y * v01z - v01y * v0z;
+        ay = v0z * v01x - v01z * v0x;
+        az = v0x * v01y - v01x * v0y;
+
+        return this.setFromAxis3f(q,ax,ay,az,angle);
+    },
+
+    setFromAxis3f : function(q,x,y,z,angle){
+        angle *= 0.5;
+
+        var length = Math.sqrt(x * x + y * y + z * z);
+
+        if(length){
+            length = 1.0 / length;
+            x *= length;
+            y *= length;
+            z *= length;
+        }
+
+        var sin = Math.sin(angle);
+        q[0] = x * sin;
+        q[1] = y * sin;
+        q[2] = z * sin;
+        q[3] = Math.cos(angle);
+
+        return q;
+    },
+
+    setFromAxis : function(q,v,angle){
+        this.setFromAxis3f(q,v[0],v[1],v[2],angle);
+    },
+
+    setFromAngles : function(q,x,y,z){
+        var cyaw, cpitch, croll, syaw, spitch, sroll;
+        var cyawcpitch, syawspitch, cyawspitch, syawcpitch;
+
+        cyaw = Math.cos(z * 0.5);
+        cpitch = Math.cos(y * 0.5);
+        croll = Math.cos(x * 0.5);
+        syaw = Math.sin(z * 0.5);
+        spitch = Math.sin(y * 0.5);
+        sroll = Math.sin(x * 0.5);
+
+        cyawcpitch = cyaw * cpitch;
+        syawspitch = syaw * spitch;
+        cyawspitch = cyaw * spitch;
+        syawcpitch = syaw * cpitch;
+
+        q[0] = cyawcpitch * croll + syawspitch * sroll;
+        q[1] = cyawcpitch * sroll - syawspitch * croll;
+        q[2] = cyawspitch * croll + syawcpitch * sroll;
+        q[3] = syawcpitch * croll - cyawspitch * sroll;
+
+        return q;
+    },
+
+
+    length: function (q) {
+        var x = q[0], y = q[1], z = q[2], w = q[3];
+        return Math.sqrt(x * x + y * y + z * z + w * w);
+    },
+
+    lengthSq : function(q){
+        var x = q[0], y = q[1], z = q[2], w = q[3];
+        return x * x + y * y + z * z + w * w;
+    },
+
+
+    conjugate : function(q){
+        q[0] *= -1;
+        q[1] *= -1;
+        q[2] *= -1;
+    },
+
+    getMatrix : function(q,m){
+        m = m || new Float32Array(16);
+
+        var x = q[0], y = q[1], z = q[2], w = q[3];
+        var x2 = x * x, y2 = y * y, z2 = z * z;
+        var xy = x * y, xz = x * z, yz = y * z;
+        var wx = w * x, wy = w * y, wz = w * z;
+
+        m[ 0] = 1.0 - 2.0 * (y2 + z2);
+        m[ 1] = 2.0 * (xy - wz);
+        m[ 2] = 2.0 * (xz + wy);
+
+        m[ 4] = 2.0 * (xy + wz);
+        m[ 5] = 1.0 - 2.0 * (x2 + z2);
+        m[ 6] = 2.0 * (yz - wx);
+
+        m[ 8] = 2.0 * (xz - wy);
+        m[ 9] = 2.0 * (yz + wx);
+        m[10] = 1.0 - 2.0 * (x2 + y2);
+
+        m[ 3] = m[ 7] = m[11] = 0.0;
+        m[12] = m[13] = m[14] = 0.0;
+
+        m[15] = 1.0;
+
+        return m;
+    }
+
+
+
+};
+
+/*
 var Quaternion = {
     create: function (n, v) {
         return new Float32Array([n, v[0], v[1], v[2]]);
@@ -37,6 +278,8 @@ var Quaternion = {
     scalar: function (q) {
         return q[0];
     },
+
+
 
 
     add: function (q0, q1) {
@@ -110,6 +353,10 @@ var Quaternion = {
         var l = Math.sqrt(x * x + y * y + z * z);
 
         return l != 0 ? new Float32Array([x / l, y / l, z / l]) : new Float32Array([0, 0, 0]);
+    },
+
+    toMatrix : function(v,m){
+
     },
 
     //TODO: INLINE ALL!!
@@ -188,5 +435,6 @@ var Quaternion = {
         return v;
     }
 };
+*/
 
 module.exports = Quaternion;
