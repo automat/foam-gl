d50b65f9a35228aa7cc3378c40318b61e8440a69
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-24 15:59:56 +0200

    moved to colour obj

--- src/foam/util/Color.js
+++ src/foam/util/Color.js
@@ -1,123 +1,63 @@
-var fMath = require('../math/Math');
+function Color(r,g,b,a) {
+    this.r = r || 0;
+    this.g = g || 0;
+    this.b = b || 0;
+    this.a = a || 1;
+}
 
-module.exports = {
-    SIZE: 4,
-
-    BLACK: function () {
-        return new Float32Array([0, 0, 0, 1])
-    },
-    WHITE: function () {
-        return new Float32Array([1, 1, 1, 1])
-    },
-    RED: function () {
-        return new Float32Array([1, 0, 0, 1])
-    },
-    GREEN: function () {
-        return new Float32Array([0, 1, 0, 1])
-    },
-    BLUE: function () {
-        return new Float32Array([0, 0, 1, 1])
-    },
-
-    create: function (r, g, b, a) {
-        return new Float32Array([ r, g, b, a]);
-    },
-    copy: function (c) {
-        return this.create(c[0], c[1], c[2], c[3]);
-    },
-
-    set: function (c0, c1) {
-        c0[0] = c1[0];
-        c0[1] = c1[1];
-        c0[2] = c1[2];
-        c0[3] = c1[3];
-
-        return c0;
-    },
-
-    set4f: function (c, r, g, b, a) {
-        c[0] = r;
-        c[1] = g;
-        c[2] = b;
-        c[3] = a;
-
-        return c;
-    },
-
-    set3f: function (c, r, g, b) {
-        c[0] = r;
-        c[1] = g;
-        c[2] = b;
-        c[3] = 1.0;
-
-        return c;
-    },
-
-    set2f: function (c, k, a) {
-        c[0] = c[1] = c[2] = k;
-        c[3] = a;
-
-        return c;
-    },
-
-    set1f: function (c, k) {
-        c[0] = c[1] = c[2] = k;
-        c[3] = 1.0;
-
-        return c;
-    },
-
-    set4i: function (c, r, g, b, a) {
-        return this.set4f(c, r / 255.0, g / 255.0, b / 255.0, a);
-    },
-    set3i: function (c, r, g, b) {
-        return this.set3f(c, r / 255.0, g / 255.0, b / 255.0);
-    },
-    set2i: function (c, k, a) {
-        return this.set2f(c, k / 255.0, a);
-    },
-    set1i: function (c, k) {
-        return this.set1f(c, k / 255.0);
-    },
-    toArray: function (c) {
-        return c.toArray();
-    },
-    toString: function (c) {
-        return '[' + c[0] + ',' + c[1] + ',' + c[2] + ',' + c[3] + ']';
-    },
-
-    interpolated: function (c0, c1, f) {
-        var c = new Float32Array(4),
-            fi = 1.0 - f;
-
-        c[0] = c0[0] * fi + c1[0] * f;
-        c[1] = c0[1] * fi + c1[1] * f;
-        c[2] = c0[2] * fi + c1[2] * f;
-        c[3] = c0[3] * fi + c1[3] * f;
-
-        return c;
-    },
-
-    equals: function (c0, c1) {
-        return _Math.isFloatEqual(c0[0], c1[0]) &&
-            _Math.isFloatEqual(c0[1], c1[1]) &&
-            _Math.isFloatEqual(c0[2], c1[2]) &&
-            _Math.isFloatEqual(c0[3], c1[3]);
-    },
-
-    makeColorArrayf: function (r, g, b, a, length) {
-        var arr = new Float32Array(length * 4), i = 0;
-        while (i < arr.length) {
-            arr[i + 0] = r;
-            arr[i + 1] = g;
-            arr[i + 2] = b;
-            arr[i + 3] = a;
-            i += 4;
-        }
-        return arr;
-    },
-
-    makeColorArray: function (color, length) {
-        return this.makeColorArrayf(color[0], color[1], color[2], color[3], length);
-    }
+Color.prototype.setf = function(r,g,b,a){
+    this.r = r;
+    this.g = g || 0;
+    this.b = b || 0;
+    this.a = a || 1;
+    return this;
 };
+
+Color.prototype.set = function(color){
+    return this.setf(color.r,color.g,color.b,color.a);
+};
+
+Color.prototype.copy = function(color){
+    return (color || new Color()).set(color);
+};
+
+Color.prototype.equals = function(color){
+    return this.r == color.r &&
+           this.g == color.g &&
+           this.b == color.b &&
+           this.a == color.a;
+};
+
+Color.prototype.toFloat32Array = function(arr){
+    arr = arr || new Float32Array(4);
+    arr[0] = this.r;
+    arr[1] = this.g;
+    arr[2] = this.b;
+    arr[3] = this.a;
+    return arr;
+};
+
+Color.white = function(){
+    return new Color(1,1,1,1);
+};
+
+Color.black = function(){
+    return new Color(1,0,0,1);
+};
+
+Color.red = function(){
+    return new Color(1,0,0,1);
+};
+
+Color.green = function(){
+    return new Color(0,1,0,1);
+};
+
+Color.blue = function(){
+    return new Color(0,0,1,1);
+};
+
+
+
+
+module.exports = Color;
