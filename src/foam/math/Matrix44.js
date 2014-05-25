d9b27d69b0e2b5147f77c4935d387a7c4cf5e0fe
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-25 11:50:46 +0200

    added prototype scale/translate/rotate

--- src/foam/math/Matrix44.js
+++ src/foam/math/Matrix44.js
@@ -53,6 +53,169 @@ Matrix44.prototype.identity = function(){
     return this;
 };
 
+Matrix44.prototype.scale = function(sx,sy,sz){
+    var m = this.m;
+
+    m[ 0] = sx;
+    m[ 5] = sy;
+    m[10] = sz;
+
+    return this;
+};
+
+Matrix44.prototype.translate = function(tx,ty,tz){
+    var m = this.m;
+
+    m[12] = tx;
+    m[13] = ty;
+    m[14] = tz;
+
+    return this;
+};
+
+Matrix44.prototype.rotateX = function(a){
+    var m = this.m;
+
+    var sin = Math.sin(a),
+        cos = Math.cos(a);
+
+    m[5] = cos;
+    m[6] = -sin;
+    m[9] = sin;
+    m[10] = cos;
+
+    return this;
+};
+
+Matrix44.prototype.rotateY = function(a){
+    var m = this.m;
+
+    var sin = Math.sin(a),
+        cos = Math.cos(a);
+
+    m[0] = cos;
+    m[2] = sin;
+    m[8] = -sin;
+    m[10] = cos;
+
+    return this;
+};
+
+Matrix44.prototype.rotateZ = function(a){
+    var m = this.m;
+
+    var sin = Math.sin(a),
+        cos = Math.cos(a);
+
+    m[0] = cos;
+    m[1] = sin;
+    m[4] = -sin;
+    m[5] = cos;
+
+    return this;
+};
+
+Matrix44.prototype.rotate = function(ax,ay,az){
+    var m = this.m;
+
+    var cosx = Math.cos(ax),
+        sinx = Math.sin(ax),
+        cosy = Math.cos(ay),
+        siny = Math.sin(ay),
+        cosz = Math.cos(az),
+        sinz = Math.sin(az);
+
+    m[ 0] = cosy * cosz;
+    m[ 1] = -cosx * sinz + sinx * siny * cosz;
+    m[ 2] = sinx * sinz + cosx * siny * cosz;
+
+    m[ 4] = cosy * sinz;
+    m[ 5] = cosx * cosz + sinx * siny * sinz;
+    m[ 6] = -sinx * cosz + cosx * siny * sinz;
+
+    m[ 8] = -siny;
+    m[ 9] = sinx * cosy;
+    m[10] = cosx * cosy;
+
+
+    return this;
+};
+
+Matrix44.prototype.rotateOnAxis = function(rot,x,y,z){
+    var len = Math.sqrt(x * x + y * y + z * z);
+
+    if (Math.sqrt(x * x + y * y + z * z) < 0.0001) {
+        return null;
+    }
+
+    var m = this.m;
+
+    var s, c, t;
+    var a00, a01, a02, a03,
+        a10, a11, a12, a13,
+        a20, a21, a22, a23,
+        b00, b01, b02,
+        b10, b11, b12,
+        b20, b21, b22;
+
+
+    len = 1 / len;
+
+    x *= len;
+    y *= len;
+    z *= len;
+
+    s = Math.sin(rot);
+    c = Math.cos(rot);
+    t = 1 - c;
+
+    a00 = a11 = a22 = 1;
+    a01 = a02 = a03 = a10 = a12 = a13 = a20 = a21 = a23 = 0;
+
+    b00 = x * x * t + c;
+    b01 = y * x * t + z * s;
+    b02 = z * x * t - y * s;
+    b10 = x * y * t - z * s;
+    b11 = y * y * t + c;
+    b12 = z * y * t + x * s;
+    b20 = x * z * t + y * s;
+    b21 = y * z * t - x * s;
+    b22 = z * z * t + c;
+
+    m[0 ] = a00 * b00 + a10 * b01 + a20 * b02;
+    m[1 ] = a01 * b00 + a11 * b01 + a21 * b02;
+    m[2 ] = a02 * b00 + a12 * b01 + a22 * b02;
+    m[3 ] = a03 * b00 + a13 * b01 + a23 * b02;
+    m[4 ] = a00 * b10 + a10 * b11 + a20 * b12;
+    m[5 ] = a01 * b10 + a11 * b11 + a21 * b12;
+    m[6 ] = a02 * b10 + a12 * b11 + a22 * b12;
+    m[7 ] = a03 * b10 + a13 * b11 + a23 * b12;
+    m[8 ] = a00 * b20 + a10 * b21 + a20 * b22;
+    m[9 ] = a01 * b20 + a11 * b21 + a21 * b22;
+    m[10] = a02 * b20 + a12 * b21 + a22 * b22;
+    m[11] = a03 * b20 + a13 * b21 + a23 * b22;
+
+    return this;
+};
+
+Matrix44.prototype.rotateOnB = function(u,v,w){
+    var m = this.m;
+
+    m[ 0] = u.x;
+    m[ 1] = u.y;
+    m[ 2] = u.z;
+
+    m[ 4] = v.x;
+    m[ 5] = v.y;
+    m[ 6] = v.z;
+
+    m[ 8] = w.x;
+    m[ 9] = w.y;
+    m[10] = w.z;
+
+    return m;
+};
+
 Matrix44.createScale = function(sx, sy, sz, mat44){
     mat44 = mat44 || new Matrix44();
     var m = mat44.m;
