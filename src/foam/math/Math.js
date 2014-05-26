9ac6ac99d123b62aacff552387df4566c16af44e
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-26 21:16:32 +0200

    moved random funcs to Random

--- src/foam/math/Math.js
+++ src/foam/math/Math.js
@@ -49,42 +49,6 @@ var _Math = {
         return a0 * b + b0 * v0 + c0 * v1 + d0 * c;
     },
 
-    randomFloat: function () {
-        var r;
-
-        switch (arguments.length) {
-            case 0:
-                r = Math.random();
-                break;
-            case 1:
-                r = Math.random() * arguments[0];
-                break;
-            case 2:
-                r = arguments[0] + (arguments[1] - arguments[0]) * Math.random();
-                break;
-        }
-
-        return r;
-    },
-
-    randomInteger: function () {
-        var r;
-
-        switch (arguments.length) {
-            case 0:
-                r = 0.5 + Math.random();
-                break;
-            case 1:
-                r = 0.5 + Math.random() * arguments[0];
-                break;
-            case 2:
-                r = arguments[0] + ( 1 + arguments[1] - arguments[0]) * Math.random();
-                break;
-        }
-
-        return Math.floor(r);
-    },
-
     constrain: function () {
         var r;
 
---
+++ src/foam/math/Random.js
@@ -0,0 +1,39 @@
+var Random = {
+    randomFloat: function () {
+        var r;
+
+        switch (arguments.length) {
+            case 0:
+                r = Math.random();
+                break;
+            case 1:
+                r = Math.random() * arguments[0];
+                break;
+            case 2:
+                r = arguments[0] + (arguments[1] - arguments[0]) * Math.random();
+                break;
+        }
+
+        return r;
+    },
+
+    randomInteger: function () {
+        var r;
+
+        switch (arguments.length) {
+            case 0:
+                r = 0.5 + Math.random();
+                break;
+            case 1:
+                r = 0.5 + Math.random() * arguments[0];
+                break;
+            case 2:
+                r = arguments[0] + ( 1 + arguments[1] - arguments[0]) * Math.random();
+                break;
+        }
+
+        return Math.floor(r);
+    }
+};
+
+module.exports = Random;
