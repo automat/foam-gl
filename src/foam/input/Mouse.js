8aef47452f3b1154351dcbfb5c740d8475984d91
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-28 19:21:00 +0200

    update mouse, moved inputs to /input

--- src/foam/input/Mouse.js
+++ src/foam/input/Mouse.js
@@ -2,7 +2,9 @@ var _Error = require('../system/common/Error'),
     Vec2 = require('../math/Vec2');
 
 function Mouse() {
-    if (Mouse.__instance)throw new Error(Error.CLASS_IS_SINGLETON);
+    if (Mouse.__instance){
+        throw new Error(Error.CLASS_IS_SINGLETON);
+    }
 
     this._position = new Vec2();
     this._positionLast = new Vec2();
@@ -13,30 +15,38 @@ function Mouse() {
     Mouse.__instance = this;
 }
 
-Mouse.prototype.getPosition = function () {
-    return this._position;
+Mouse.prototype.getPosition = function (v) {
+    return (v || new Vec2()).set(this._position);
 };
-Mouse.prototype.getPositionLast = function () {
-    return this._positionLast;
+
+Mouse.prototype.getPositionLast = function (v) {
+    return (v || new Vec2()).set(this._positionLast);
 };
+
 Mouse.prototype.getX = function () {
     return this._position.x;
 };
+
 Mouse.prototype.getY = function () {
     return this._position.y;
 };
+
 Mouse.prototype.getXLast = function () {
     return this._positionLast.x;
 };
+
 Mouse.prototype.getYLast = function () {
     return this._positionLast.y;
 };
+
 Mouse.prototype.getState = function () {
     return this._state;
 };
+
 Mouse.prototype.getStateLast = function () {
     return this._stateLast;
 };
+
 Mouse.prototype.getWheelDelta = function () {
     return this._wheelDelta;
 };
--- src/foam/input/MouseState.js
+++ src/foam/input/MouseState.js
@@ -1,8 +1,6 @@
-var MouseState =
-{
+var MouseState = {
     MOUSE_DOWN  : 'mousedown',
     MOUSE_UP    : 'mouseup',
-    MOUSE_MOVE  : 'mousemove',
     MOUSE_DRAG  : 'mousedrag',
     MOUSE_MOVE  : 'mousemove',
     MOUSE_WHEEL : 'mousewheel'
