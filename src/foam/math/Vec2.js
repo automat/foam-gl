f5ad554302ce53cb27cb2e48fe0e0e437b4a989e
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-28 19:21:10 +0200

    added set/setf

--- src/foam/math/Vec2.js
+++ src/foam/math/Vec2.js
@@ -3,4 +3,16 @@ function Vec2(x,y){
     this.y = y || 0;
 }
 
+Vec2.prototype.set = function(v){
+    this.x = v.x;
+    this.y = v.y;
+    return this;
+};
+
+Vec2.prototype.setf = function(x,y){
+    this.x = x;
+    this.y = y;
+    return this;
+};
+
 module.exports = Vec2;
