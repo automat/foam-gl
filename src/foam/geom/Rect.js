e5b02833f6debd9c0ac64c029a5e91aefd8e0aa0
Henryk Wollik <olafyoorsen@hotmail.com>
2014-05-28 19:21:28 +0200

    Rect initial

---
+++ src/foam/geom/Rect.js
@@ -0,0 +1,74 @@
+var Vec2 = require('../math/Vec2'),
+    Vec3 = require('../math/Vec3');
+
+function Rect() {
+    this.x0 = 0;
+    this.y0 = 0;
+    this.x1 = 0;
+    this.y1 = 0;
+}
+
+Rect.prototype.set = function(rect){
+    this.x0 = rect.x0;
+    this.y0 = rect.y0;
+    this.x1 = rect.x1;
+    this.y1 = rect.y1;
+};
+
+Rect.prototype.setPosition = function(v){
+    var width = this.getWidth(),
+        height = this.getHeight();
+    this.x0 = v.x;
+    this.y0 = v.y;
+    this.x1 = this.x0 + width;
+    this.y1 = this.y0 + height;
+};
+
+Rect.prototype.setPositionf = function(x,y){
+    var width = this.getWidth(),
+        height = this.getHeight();
+    this.x0 = x;
+    this.y0 = y;
+    this.x1 = this.x0 + width;
+    this.y1 = this.y0 + height;
+};
+
+Rect.prototype.getPosition = function(v){
+    return (v || new Vec2()).set(this.x0,this.y0);
+};
+
+Rect.prototype.setSize = function(v){
+    this.x1 = this.x0 + v.x;
+    this.y1 = this.y0 + v.y;
+};
+
+Rect.prototype.setSizef = function(width,height){
+    this.x1 = this.x0 + width;
+    this.y1 = this.y0 + height;
+};
+
+Rect.prototype.setWidth = function(width){
+    this.x1 = this.x0 + width;
+};
+
+Rect.prototype.setHeight = function(height){
+    this.y1 = this.y0 + height;
+};
+
+Rect.prototype.getWidth = function(){
+    return this.x1 - this.x0;
+};
+
+Rect.prototype.getHeight = function(){
+    return this.y1 - this.y0;
+};
+
+Rect.prototype.getSize = function(v){
+    return (v || new Vec2()).set(this.getWidth(),this.getHeight());
+};
+
+Rect.prototype.getCenter = function(v){
+    return (v || new Vec2()).setf((this.x0 + this.x1) * 0.5,(this.y0 + this.y1) * 0.5);
+};
+
+module.exports = Rect;
