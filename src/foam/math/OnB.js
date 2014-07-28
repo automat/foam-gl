da45ff22e6a84512be9c99be7c6c50bf360ce97d
Henryk Wollik <olafyoorsen@hotmail.com>
2014-07-28 10:36:17 +0200

    initial doc

--- src/foam/math/OnB.js
+++ src/foam/math/OnB.js
@@ -1,17 +1,36 @@
 var Vec3 = require('./Vec3'),
     Matrix44 = require('./Matrix44');
 
+/**
+ * Representation of an orthogonal basis.
+ * @param {Vec3} u - u
+ * @param {Vec3} v - v
+ * @param {Vec3} w - w
+ * @constructor
+ */
+
 function OnB(u,v,w){
     this.u = Vec3.zAxis();
     this.v = Vec3.yAxis();
     this.w = Vec3.xAxis();
 }
 
+/**
+ * Set onb from another onb.
+ * @param {Onb} onb - Another onb
+ */
+
 OnB.prototype.set = function(onb){
     this.u.set(onb.u);
     this.v.set(onb.v);
     this.w.set(onb.w);
 };
+
+/**
+ * Returns a transformation matrix from the onb.
+ * @param {Matrix44} [mat] - Out matrix
+ * @returns {Matrix44}
+ */
 
 OnB.prototype.getMatrix = function(mat){
     if(mat){
