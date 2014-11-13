423379841008d43b0577864a4ee12bb36d98383a
Henryk Wollik <olafyoorsen@hotmail.com>
2014-11-13 15:38:23 +0100

    default Material initial

--- src/foam/gl/Material.js
+++ src/foam/gl/Material.js
@@ -1,95 +1,101 @@
-var Color = require('../util/Color');
+var _gl     = require('./gl'),
+    Color   = require('../util/Color'),
+    Program = require('./Program');
 
 function Material(ambient, diffuse, specular, shininess, emission) {
-    ambient   = ambient || Color.create(1.0, 0.5, 0.5, 1.0);
-    diffuse   = diffuse   || Color.BLACK();
-    specular  = specular || Color.BLACK();
+    ambient   = ambient || new Color(1.0, 0.5, 0.5, 1.0);
+    diffuse   = diffuse   || Color.black();
+    specular  = specular || Color.black();
     shininess = shininess || 10.0;
-    emission  = emission || Color.BLACK;
+    emission  = emission || Color.black();
+
+    this._gl = _gl.get();
+
+    this._enabled = true;
 
     this.emission = emission;
     this.ambient = ambient;
     this.diffuse = diffuse;
     this.specular = specular;
     this.shininess = shininess;
+
+    this._tempF32 = new Float32Array(4);
+
+    this._uniformLocationEmissionKey  = Program.UNIFORM_MATERIAL_STRUCT + '.' + Program.UNIFORM_MATERIAL_STRUCT_EMISSION;
+    this._uniformLocationAmbientKey   = Program.UNIFORM_MATERIAL_STRUCT + '.' + Program.UNIFORM_MATERIAL_STRUCT_AMBIENT;
+    this._uniformLocationDiffuseKey   = Program.UNIFORM_MATERIAL_STRUCT + '.' + Program.UNIFORM_MATERIAL_STRUCT_DIFFUSE;
+    this._uniformLocationSpecularKey  = Program.UNIFORM_MATERIAL_STRUCT + '.' + Program.UNIFORM_MATERIAL_STRUCT_SPECULAR;
+    this._uniformLocationShininessKey = Program.UNIFORM_MATERIAL_STRUCT + '.' + Program.UNIFORM_MATERIAL_STRUCT_SHININESS;
+
+    this._uniformLocationEmission =
+        this._uniformLocationAmbient =
+            this._uniformLocationDiffuse =
+                this._uniformLocationSpecular =
+                    this._uniformLocationShininess = null;
 }
 
-Material.prototype.setEmission = function (color) {
-    this.emission = color;
-};
-Material.prototype.setEmission3f = function (r, g, b) {
-    this.emission[0] = r;
-    this.emission[1] = g;
-    this.emission[2] = b;
-};
-Material.prototype.setEmission4f = function (r, g, b, a) {
-    this.emission[0] = r;
-    this.emission[1] = g;
-    this.emission[2] = b;
-    this.emission[3] = a;
+Material.prototype.enable = function(){
+    this._enabled = true;
 };
 
-Material.prototype.setAmbient = function (color) {
-    this.ambient = color;
-};
-Material.prototype.setAmbient3f = function (r, g, b) {
-    this.ambient[0] = r;
-    this.ambient[1] = g;
-    this.ambient[2] = b;
-};
-Material.prototype.setAmbient4f = function (r, g, b, a) {
-    this.ambient[0] = r;
-    this.ambient[1] = g;
-    this.ambient[2] = b;
-    this.ambient[3] = a;
+Material.prototype.disable = function(){
+    this._enabled = false;
 };
 
-Material.prototype.setDiffuse = function (color) {
-    this.diffuse = color;
-};
+var prevProgram = null;
 
-Material.prototype.setDiffuse3f = function (r, g, b) {
-    this.diffuse[0] = r;
-    this.diffuse[1] = g;
-    this.diffuse[2] = b;
-};
+Material.prototype.apply = function(){
+    if(!this._enabled){
+        return;
+    }
+    var gl = this._gl;
+    var program = gl.getParameter(gl.CURRENT_PROGRAM);
 
-Material.prototype.setDiffuse4f = function (r, g, b, a) {
-    this.diffuse[0] = r;
-    this.diffuse[1] = g;
-    this.diffuse[2] = b;
-    this.diffuse[3] = a;
-};
+    if(program != prevProgram){
+        this._uniformLocationEmission = gl.getUniformLocation(program,this._uniformLocationEmissionKey);
+        this._uniformLocationAmbient  = gl.getUniformLocation(program,this._uniformLocationAmbientKey);
+        this._uniformLocationDiffuse  = gl.getUniformLocation(program,this._uniformLocationDiffuseKey);
+        this._uniformLocationSpecular = gl.getUniformLocation(program,this._uniformLocationSpecularKey);
+        this._uniformLocationShininess= gl.getUniformLocation(program,this._uniformLocationShininessKey);
+        prevProgram = program;
+    }
 
-Material.prototype.setSpecular = function (color) {
-    this.specular = color;
-};
-Material.prototype.setSpecular3f = function (r, g, b) {
-    this.specular[0] = r;
-    this.specular[1] = g;
-    this.specular[2] = b;
-};
-Material.prototype.setSpecular4f = function (r, g, b, a) {
-    this.specular[0] = r;
-    this.specular[1] = g;
-    this.specular[2] = b;
-    this.specular[3] = a;
-};
+    var emission = this.emission,
+        ambient = this.ambient,
+        diffuse = this.diffuse,
+        specular = this.specular;
 
-Material.prototype.getEmission = function () {
-    return Color.copy(this.emission);
-};
-Material.prototype.getAmbient = function () {
-    return Color.copy(this.ambient);
-};
-Material.prototype.getDiffuse = function () {
-    return Color.copy(this.diffuse);
-};
-Material.prototype.getSpecular = function () {
-    return Color.copy(this.specular);
-};
-Material.prototype.getShininess = function () {
-    return this.shininess;
-};
+    var tempF32 = this._tempF32;
+
+    tempF32[0] = emission.r;
+    tempF32[1] = emission.g;
+    tempF32[2] = emission.b;
+    tempF32[3] = emission.a;
+
+    gl.uniform4fv(this._uniformLocationEmission, tempF32);
+
+    tempF32[0] = ambient.r;
+    tempF32[1] = ambient.g;
+    tempF32[2] = ambient.b;
+    tempF32[3] = ambient.a;
+
+    gl.uniform4fv(this._uniformLocationAmbient, tempF32);
+
+    tempF32[0] = diffuse.r;
+    tempF32[1] = diffuse.g;
+    tempF32[2] = diffuse.b;
+    tempF32[3] = diffuse.a;
+
+    gl.uniform4fv(this._uniformLocationDiffuse, tempF32);
+
+    tempF32[0] = specular.r;
+    tempF32[1] = specular.g;
+    tempF32[2] = specular.b;
+    tempF32[3] = specular.a;
+
+    gl.uniform4fv(this._uniformLocationSpecular, tempF32);
+
+    gl.uniform1f(this._uniformLocationShininess, this.shininess);
+}
 
 module.exports = Material;
