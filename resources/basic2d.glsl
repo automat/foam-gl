e1b1b424e395bfe0239dea2b97a60faca9b28786
Henryk Wollik <olafyoorsen@hotmail.com>
2014-11-22 17:16:50 +0100

    moved basic shaders to ./resources

--- resources/basic2d.glsl
+++ resources/basic2d.glsl
@@ -2,41 +2,21 @@
 precision highp float;
 
 attribute vec3 aVertexPosition;
-attribute vec4 aVertexColor;
-attribute vec2 aTexcoord;
+uniform   mat4 uProjectionMatrix;
+uniform   mat4 uModelViewMatrix;
 
-varying   vec4 vVertexColor;
-varying   vec2 vTexcoord;
-
-uniform mat4 uProjectionMatrix;
-uniform mat4 uModelViewMatrix;
-
-uniform float uPointSize;
 
 void main(){
-    vVertexColor = aVertexColor;
-    vTexcoord    = aTexcoord;
-
     gl_Position = uProjectionMatrix * uModelViewMatrix * vec4(aVertexPosition,1.0);
-    gl_PointSize = uPointSize;
+    gl_PointSize = 3.0;
 }
 #endif
 
 
 #ifdef FRAGMENT_SHADER
 precision highp float;
-
-uniform sampler2D uTexture;
-
-varying vec4  vVertexColor;
-varying vec2  vTexcoord;
-
-uniform float uUseTexture;
-
-uniform vec4  uColor;
-uniform float uUseUniformColor;
-
+uniform vec4 uColor;
 void main(){
-    gl_FragColor = (vVertexColor * (1.0 - uUseUniformColor) + uColor * uUseUniformColor) * (1.0 - uUseTexture) + texture2D(uTexture,vTexcoord) * uUseTexture;
+    gl_FragColor = uColor;
 }
 #endif
